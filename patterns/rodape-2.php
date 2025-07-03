5ad38908bc5bea909947747bab2aa0ba94971156
mateuswetah <mateus.m.luna@gmail.com>
2025-07-03 14:58:49 -0300

    Remove referência quebrada à imagem no rodapé.

--- patterns/rodape-2.php
+++ patterns/rodape-2.php
@@ -13,10 +13,7 @@
 <div class="wp-block-columns has-palette-color-8-color has-palette-color-10-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--10);padding-right:0;padding-bottom:var(--wp--preset--spacing--10);padding-left:0"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group"><!-- wp:site-logo /-->
-
-<!-- wp:image {"id":26813,"width":"260px","height":"auto","sizeSlug":"tainacan-medium-full","linkDestination":"none"} -->
-<figure class="wp-block-image size-tainacan-medium-full is-resized"><img src="https://localhost/wp-content/uploads/2025/05/06ed25203376141.67deb3467b9ea-205x85.jpg" alt="" class="wp-image-26813" style="width:260px;height:auto"/></figure>
-<!-- /wp:image --></div>
+</div>
 <!-- /wp:group -->
 
 <!-- wp:site-title {"style":{"elements":{"link":{"color":{"text":"var:preset|color|palette-color-8"},":hover":{"color":{"text":"var:preset|color|palette-color-8"}}}},"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"500"}},"textColor":"palette-color-8"} /--></div>
