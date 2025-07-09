ffd93bbf9c27cf1313de5d62738895f0006dad44
mateuswetah <mateus.m.luna@gmail.com>
2025-07-09 16:08:36 -0300

    Checagens de segurança em chamadas de funções terceiras.

--- inc/global.php
+++ inc/global.php
@@ -4,34 +4,38 @@
  * carregue o CSS global do tema a partir de configurações
  * adicionais do menu Personalizar.
  */
-blc_call_fnc(['fnc' => 'blocksy_output_spacing'], [
-	'css' => $css,
-	'tablet_css' => $tablet_css,
-	'mobile_css' => $mobile_css,
-    'selector' => ':root',
-	'property' => 'iphan-blocksy-border-radius',
-	'value' => blocksy_get_theme_mod(
-		'iphan_blocksy_general_border_radius',
-		blocksy_spacing_value()
-	),
-	'empty_value' => 4,
-]);
+if ( function_exists('blc_call_fnc') ) {
+	blc_call_fnc(['fnc' => 'blocksy_output_spacing'], [
+		'css' => $css,
+		'tablet_css' => $tablet_css,
+		'mobile_css' => $mobile_css,
+		'selector' => ':root',
+		'property' => 'iphan-blocksy-border-radius',
+		'value' => blocksy_get_theme_mod(
+			'iphan_blocksy_general_border_radius',
+			blocksy_spacing_value()
+		),
+		'empty_value' => 4,
+	]);
+}
 
-blocksy_output_background_css([
-	'selector' => '.tainacan-item-single-page .tainacan-item-single .iphan-blocksy-metadata-section--tabs .tainacan-item-section--metadata',
-	'css' => $css,
-	'tablet_css' => $tablet_css,
-	'mobile_css' => $mobile_css,
-	'value' => blocksy_get_theme_mod(
-		'iphan_blocksy_item_tabs_background',
-		blocksy_background_default_value([
-			'backgroundColor' => [
-				'default' => [
-					'color' => 'var(--theme-palette-color-8)'
+if ( function_exists('blocksy_output_background_css') ) {
+	blocksy_output_background_css([
+		'selector' => '.tainacan-item-single-page .tainacan-item-single .iphan-blocksy-metadata-section--tabs .tainacan-item-section--metadata',
+		'css' => $css,
+		'tablet_css' => $tablet_css,
+		'mobile_css' => $mobile_css,
+		'value' => blocksy_get_theme_mod(
+			'iphan_blocksy_item_tabs_background',
+			blocksy_background_default_value([
+				'backgroundColor' => [
+					'default' => [
+						'color' => 'var(--theme-palette-color-8)'
+					],
 				],
-			],
-		])
-	),
-	'responsive' => true,
-	'forced_background_image' => true
-]);
+			])
+		),
+		'responsive' => true,
+		'forced_background_image' => true
+	]);
+}
